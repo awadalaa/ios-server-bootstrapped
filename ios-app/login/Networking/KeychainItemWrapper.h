a52250755af6e82769f87473182ad17804e16b60
Alaa Awad <alaa.awad.mail@gmail.com>
2015-03-08 22:29:46 -0400

    boo

---
+++ ios-app/login/Networking/KeychainItemWrapper.h
@@ -0,0 +1,13 @@
+//
+//  KeychainItemWrapper.h
+//  login
+//
+//  Created by Alaa Awad on 3/8/15.
+//  Copyright (c) 2015 Paul Jackson. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface KeychainItemWrapper : NSObject
+
+@end
---
+++ ios-app/login/Networking/KeychainItemWrapper.m
@@ -0,0 +1,13 @@
+//
+//  KeychainItemWrapper.m
+//  login
+//
+//  Created by Alaa Awad on 3/8/15.
+//  Copyright (c) 2015 Paul Jackson. All rights reserved.
+//
+
+#import "KeychainItemWrapper.h"
+
+@implementation KeychainItemWrapper
+
+@end
