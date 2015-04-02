8401dafa4eb8b0b151811861dc50021e057f10bc
Alaa Awad <alaa.awad.mail@gmail.com>
2015-04-02 00:22:20 -0400

    SQPostToServerViewController added

---
+++ ios-app/login/ViewControllers/SQPostToServerViewController.h
@@ -0,0 +1,15 @@
+//
+//  SQPostToServerViewController.h.h
+//  login
+//
+//  Created by Alaa M Awad on 4/1/15.
+//  Copyright (c) 2015 Technalaa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SQPostToServerViewController : UIViewController
+
+- (instancetype) initWithImage:(UIImage *)sourceImage;
+
+@end
